ac9be6dc58166ef074633b16aee6e90c0d63b930
basileusesport <kill.maldeme@gmail.com>
2024-03-30 00:34:44 +0100

    Update9

--- sources/js/rotate-valeurs.js
+++ sources/js/rotate-valeurs.js
@@ -1,5 +1,38 @@
-let div = document.querySelector(".texte");
+let chaineCaract = document.querySelector(".texte");
+let int = document.querySelector(".number-int");
+let float = document.querySelector(".number-float");
+let bool = document.querySelector(".bool");
 
-div.addEventListener("click", () => {
-    div.classList.toggle("rotated");
+let pChaineCaract = document.querySelector("#texte-p");
+let pInt = document.querySelector("#int-p");
+let pFloat = document.querySelector("#float-p");
+let pBool = document.querySelector("#bool-p");
+
+chaineCaract.addEventListener("click", () => {
+    chaineCaract.classList.toggle("rotated");
+    setTimeout(() => {
+        pChaineCaract.classList.toggle("invert");
+        pChaineCaract.innerHTML = "La chaine de caractère est symbolisée par des guillemets autour d'un texte. Elle sert à communiquer des informations écrites !"
+    }, 50);
+});
+
+int.addEventListener("click", () => {
+    int.classList.toggle("rotated");
+    setTimeout(() => {
+        pInt.classList.toggle("invert");
+    }, 50);
+});
+
+float.addEventListener("click", () => {
+    float.classList.toggle("rotated");
+    setTimeout(() => {
+        pFloat.classList.toggle("invert");
+    }, 50);
+});
+
+bool.addEventListener("click", () => {
+    bool.classList.toggle("rotated");
+    setTimeout(() => {
+        pBool.classList.toggle("invert");
+    }, 50);
 });
