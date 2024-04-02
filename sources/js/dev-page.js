0c946b44d86d3866b86695e5e4de3e228bef448a
basileusesport <kill.maldeme@gmail.com>
2024-04-02 19:07:30 +0200

    Update12

---
+++ sources/js/dev-page.js
@@ -0,0 +1,6 @@
+let btnLaunchCode = document.querySelector(".launch-code");
+let output = document.querySelector(".output-code");
+
+btnLaunchCode.addEventListener("click", () =>  {
+    output.innerHTML = "";
+});
